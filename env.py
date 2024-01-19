83151c282ff7676ebc4c78f1705e955263822420
vincent.li <vincentli2023@gmail.com>
2024-01-19 22:32:29 +0800

    bug fix

--- env.py
+++ env.py
@@ -4,4 +4,4 @@ bucket = 'vincent-li'
 tables = os.environ.get("TABLES", "defi.all_label_address").split(",")
 lark_url = os.environ.get("LARK_URL", "")
 update_interval = int(os.environ.get("UPDATE_INTERVAL_MINUTES", 60))
-uploading = int(os.environ.get("uploading", 0.0))
+uploading = int(float(os.environ.get("uploading", 0.0)))
